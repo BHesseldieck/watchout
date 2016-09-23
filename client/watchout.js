d7ab9b9f728642115fad41225f0d2ae93de35305
Bill Zito <bill.r.zito@gmail.com>
2016-09-23 10:48:59 -0700

    First commit

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,21 @@
 // start slingin' some d3 here.
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+};
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
+
+var gameBoard = d3.select('.container').append('svg:svg')
+                .attr('width', gameOptions.width)
+                .attr('height', gameOptions.height);
