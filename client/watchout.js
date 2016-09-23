830fb6ee24b5a5eb1dc1784cfe449b59dab4faeb
Bill Zito <bill.r.zito@gmail.com>
2016-09-23 14:25:24 -0700

    Tried to make drag functionality

--- client/watchout.js
+++ client/watchout.js
@@ -11,23 +11,26 @@ var gameStats = {
   bestScore: 0
 };
 
+//create axes why?
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
   y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
 };
 
+//create a board that is svg of given width and height above
 d3.select('.board').append('svg:svg')
                 .attr('width', gameOptions.width)
                 .attr('height', gameOptions.height);
 
+//set variables so that we can use them in creating enemies
 var svg = d3.select('svg');
 //+ turns it to number instead of string
 var width = +svg.attr('width');
 var height = +svg.attr('height');
-var radius = 32;
-//create enemies
+var radius = 20;
 
-var circles = d3.range(0, gameOptions.nEnemies).map(function(i) {
+//create enemiesat random x and y location
+var enemyCircles = d3.range(0, gameOptions.nEnemies).map(function(i) {
   return {
     id: i,
     x: Math.floor(Math.random() * ( width - radius * 2)),
@@ -35,15 +38,50 @@ var circles = d3.range(0, gameOptions.nEnemies).map(function(i) {
   }; 
 });
 
-svg.selectAll('circle')
-	.data(circles)
+//bind d3 data of coordinates and radius to enemy circles
+//and set to color white
+//this has to be circle because we are making circle elements
+//with a circle x and circle y and radius?
+svg.selectAll('circleEnemey')
+	.data(enemyCircles)
 	.enter().append('circle')
 		.attr('cx', function(d) { return d.x; })
 		.attr('cy', function(d) { return d.y; })
 		.attr('r', radius)
-		.style('fill', function(d, i) { return 'white'; });
+		.style('fill', 'black');
+		//can set to random color following the tutorial
 
-//remember to add dragability on these
+var player = {id: 99999, x: 100, y: 100};
+//make a player and append to svg container
+svg.append('circle')
+		.attr('cx', player.x)
+		.attr('cy', player.y)
+		.attr('r', radius)
+		.style('fill', 'red')
+		.call(d3.drag()
+			.on('start', dragStarted)
+			.on('drag', dragged)
+			.on('end', dragEnded));
+
+//make player draggable --> when dragged, call dragstarted
+//when moving, call dragged/ when done, call end
+
+
+//dragstarted happens on start--> creates active class
+//using raise to allow movement
+var dragStarted = function(d) {
+  d3.select(this).raise().classed('active', true);
+};
+
+//during drag, allows cx and cy to change to where you move them
+var dragged = function(d) {
+  d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
+};
+
+//after drag, set raise to false
+var dragEnded = function(d) {
+  d3.select(this).classed('active', false);
+};
 
 //score updates
 var updateScore = function() { 
