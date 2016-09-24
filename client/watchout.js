a61259d400ac836d83384cdee47072a548213e47
BHesseldieck <b.hesseldieck@gmail.com>
2016-09-23 17:20:39 -0700

    Complete drag and enemy movement

--- client/watchout.js
+++ client/watchout.js
@@ -34,7 +34,7 @@ var enemyCircles = d3.range(0, gameOptions.nEnemies).map(function(i) {
   return {
     id: i,
     x: Math.floor(Math.random() * ( width - radius * 2)),
-    y: Math.floor(Math.random() * ( width - radius * 2))
+    y: Math.floor(Math.random() * ( height - radius * 2))
   }; 
 });
 
@@ -42,40 +42,75 @@ var enemyCircles = d3.range(0, gameOptions.nEnemies).map(function(i) {
 //and set to color white
 //this has to be circle because we are making circle elements
 //with a circle x and circle y and radius?
-svg.selectAll('enemyCircle')
-	.data(enemyCircles)
+svg.selectAll('.enemyCircle')
+	.data(enemyCircles, function(d) { return d.id; })
 	.enter().append('circle')
+    .attr('class', 'enemyCircle')
 		.attr('cx', function(d) { return d.x; })
 		.attr('cy', function(d) { return d.y; })
 		.attr('r', radius)
 		.style('fill', 'black');
 
-var player = {id: 99999, x: 100, y: 100};
-//make a player and append to svg container
-svg.selectAll('enemyCirle')
-		.call(d3.drag()
-			.on('start', dragStarted)
-			.on('drag', dragged)
-			.on('end', dragEnded));
 
+//move to a new random location every second
+var enemyNewLocation = function() {
+  svg.selectAll('circle.enemyCircle')
+    .data(enemyCircles)
+    .transition()
+      .duration(750)
+    .attr('cx', function (d) { return d.x = Math.floor(Math.random() * ( width - radius * 2)); })
+    .attr('cy', function (d) { return d.y = Math.floor(Math.random() * ( height - radius * 2) + radius); });
+};    
+//cool thing for later: change radius every second as well
+
+//interval function that calls update function
+setInterval(enemyNewLocation, 1000);
+  //update function 
+    //calls enemyNewLocation
+    //calls playerNewLocation
+    //calls score update
+
+//players
+var player = [{id: 999, x: 300, y: 300}];
+//make a player and append to svg container
+//remember this binding!
+var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on('dragstart', function (d) { return dragstarted.bind(this)(d); })
+    .on('drag', function(d) { return dragged.bind(this)(d); })
+    .on('dragend', function(d) { return dragended.bind(this)(d); });
+
+
+svg.selectAll('.player')
+  .data(player)
+  .enter().append('circle')
+      .attr('class', 'player')
+      .attr('cx', function(d) { return d.x; })
+      .attr('cy', function(d) { return d.y; })
+      .attr('r', radius)
+      .style('fill', 'red')
+      .call(drag);
 //make player draggable --> when dragged, call dragstarted
 //when moving, call dragged/ when done, call end
 
 
 //dragstarted happens on start--> creates active class
 //using raise to allow movement
-var dragStarted = function(d) {
-  d3.select(this).raise().classed('active', true);
+var dragstarted = function(d) {
+  d3.select(this).classed('dragging', true);
+
 };
 
 //during drag, allows cx and cy to change to where you move them
+//we can't use svg.select(this) -- it gives us an error that we
+//cannot query the object... does that mean it can't get object SVGcircle from the svg? 
 var dragged = function(d) {
   d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
 };
 
 //after drag, set raise to false
-var dragEnded = function(d) {
-  d3.select(this).classed('active', false);
+var dragended = function(d) {
+  d3.select(this).classed('dragging', false);
 };
 
 //score updates
